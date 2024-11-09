b2f0623df7d0ee66147f2cba52e4a1b412a6a319
vedika-404 <vedika.pillai.2007@gmail.com>
2024-11-09 23:14:49 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,10 +3,21 @@
 window.addEventListener("scroll", function () {
     const scrollPos = window.scrollY;
     const body = document.body;
+    const fixedHeader = document.getElementById("fixed-header");
 
     if (scrollPos > 50) { // Adjust scroll threshold as needed
         body.classList.add("scrolled");
+        fixedHeader.style.top = "0"; // Show header
     } else {
         body.classList.remove("scrolled");
+        fixedHeader.style.top = "-100px"; // Hide header when scrolling back up
+    }
+
+    // Show "Oops, you just lost the 404 :(" message
+    const errorSection = document.getElementById("error-section");
+    if (scrollPos < errorSection.clientHeight) {
+        errorSection.querySelector('p:nth-child(3)').innerText = "Oops, you just lost the 404 :(";
+    } else {
+        errorSection.querySelector('p:nth-child(3)').innerText = "The resource requested could not be found on this server!";
     }
 });
